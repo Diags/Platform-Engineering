fe235f70e9b75d3fbfffb6065c3ba1b0229a2571
Diags <diaguilybouna@gmail.com>
2025-09-25 14:39:09 +0200

    Trigger CI/CD pipeline - update app.py

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 # the associated function.
 @app.route('/')
 def home():
+    # Trigger CI/CD pipeline
     return jsonify({
         "message": "Welcome to Platform Engineering API",
         "version": "1.0.0",
